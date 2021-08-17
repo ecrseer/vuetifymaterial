311d50b92014cf43bd057b0193fb33ba4ab4d363
ecrseer <juustino@live.com>
2021-08-17 19:32:04 -0300

    vue router ver

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
-import VueRouter from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import FilmeLista from "./components/FilmeLista.vue";
 
 const rotas = [
-    { path: '/foo', component: Foo },
-    { path: '/bar', component: Bar }
+    { path: '/lista',name:'FilmeLista', component: FilmeLista },
+    { path: '/', name:'HelloWorld',component: HelloWorld }
 ]
-const router = VueRouter.createRouter(rotas)
+
+const router = createRouter({
+    history:createWebHashHistory(),
+    rotas,
+})
+
+export default router;
